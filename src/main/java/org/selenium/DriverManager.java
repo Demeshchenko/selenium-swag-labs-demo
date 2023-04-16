1d273e109f7f1f6dcd496b417eef6b30bda7617f
DreneyA <demeschenko.andrey@gmail.com>
2023-04-16 20:51:53 +0300

    Add Allure reports

--- src/main/java/org/selenium/DriverManager.java
+++ src/main/java/org/selenium/DriverManager.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.dataProvider.PropertiesReaderSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
@@ -17,7 +18,9 @@ public class DriverManager {
         switch (type) {
             case "CHROME":
                 WebDriverManager.chromedriver().setup();
-                driver.set(new ChromeDriver());
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                driver.set(new ChromeDriver(options));
                 break;
             case "FIREFOX":
                 WebDriverManager.firefoxdriver().setup();
